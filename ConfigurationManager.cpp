5d5e3269ec1955de78ac1e3f93551087d14a62f2
saphalpdyl <saphalpdyl@gmail.com>
2025-02-17 00:46:07 -0600

    change: dummy mistake to have implementation in header file

--- ConfigurationManager.cpp
+++ ConfigurationManager.cpp
@@ -1,5 +1,29 @@
 //
-// Created by saphal69 on 2/17/25.
+// Created by saphalpdyl on 2/17/25.
 //
 
 #include "ConfigurationManager.h"
+#include <fstream>
+
+
+ConfigurationManager::ConfigurationManager(
+    const std::string& configurationFilename,
+    const std::string& configurationDirectory
+) {
+    _configurationFilename = configurationFilename;
+    _configurationDirectory = configurationDirectory;
+}
+
+
+PingerResult ConfigurationManager::loadConfiguration() const {
+    // cF = configuration file
+    std::ifstream cF(_configurationFilename);
+
+    if ( !cF.is_open() ) {
+        return PingerResult::ERR_CONFIGURATION_FILE_NOT_FOUND;
+    }
+
+    json data = json::parse(cF);
+
+    return PingerResult::OK;
+}
--- ConfigurationManager.h
+++ ConfigurationManager.h
@@ -5,7 +5,6 @@
 #ifndef CONFIGURATIONMANAGER_H
 #define CONFIGURATIONMANAGER_H
 
-#include <fstream>
 #include <string>
 #include <nlohmann/json.hpp>
 
@@ -14,26 +13,9 @@
 using namespace nlohmann;
 
 class ConfigurationManager {
-    ConfigurationManager(
-        const std::string& configurationFilename,
-        const std::string& configurationDirectory
-    ) {
-        _configurationFilename = configurationFilename;
-        _configurationDirectory = configurationDirectory;
-    }
+    ConfigurationManager(const std::string& configurationFilename, const std::string& configurationDirectory);
 
-    PingerResult loadConfiguration() {
-        // cF = configuration file
-        std::ifstream cF(_configurationFilename);
-
-        if ( !cF.is_open() ) {
-            return PingerResult::ERR_CONFIGURATION_FILE_NOT_FOUND;
-        }
-
-        json data = json::parse(cF);
-
-        return PingerResult::OK;
-    }
+    PingerResult loadConfiguration() const;
 
 private:
     std::string _configurationFilename;
